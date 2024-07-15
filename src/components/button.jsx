0acfa1548e969ca232d93cb0e8567341f53ffc25
kimdayeon <kdiana080918@gmail.com>
2024-07-15 23:25:42 +0900

    Add Button Component

---
+++ src/components/button.jsx
@@ -0,0 +1,53 @@
+import Button, { styled } from "styled-components";
+import { theme } from "../styles/theme";
+
+const ButtonTest = ({size, children, onClick}) => {
+    return (
+         <>  {size === "Small" && (
+                <SmallButton onClick={onClick}>{children}</SmallButton>
+            )}
+            {size === "Medium" && (
+                <MediumButton onClick={onClick}>{children}</MediumButton>
+            )}
+            {size === "Large" && (
+                <LargeButton onClick={onClick}>{children}</LargeButton>
+            )}
+            </> 
+    )
+}
+export default ButtonTest;
+
+const SmallButton = styled.div`
+    background-color: ${theme.color.main[500]};
+    width: 111px;
+    height: 52px;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-family: wanted;
+`
+
+const MediumButton = styled.div`
+    background-color: ${theme.color.main[500]};
+    width: 140px;
+    height: 56px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-family: wanted;
+`
+
+const LargeButton = styled.div`
+    background-color: ${theme.color.main[500]};
+    width: 600px;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    color: white;
+    font-family: wanted;
+`
