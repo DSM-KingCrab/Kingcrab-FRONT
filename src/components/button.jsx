f5c5201783ee4b811df1eeef54f4d09aa1eb8cdf
eksqkdrn <kdiana080918@gmail.com>
2024-07-21 02:02:38 +0900

    Add::login

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,53 +1,46 @@
 import Button, { styled } from "styled-components";
 import { theme } from "../styles/theme";
 
-const ButtonTest = ({size, children, onClick}) => {
-    return (
-         <>  {size === "Small" && (
-                <SmallButton onClick={onClick}>{children}</SmallButton>
-            )}
-            {size === "Medium" && (
-                <MediumButton onClick={onClick}>{children}</MediumButton>
-            )}
-            {size === "Large" && (
-                <LargeButton onClick={onClick}>{children}</LargeButton>
-            )}
-            </> 
-    )
-}
+const ButtonTest = ({ size, children, onClick }) => {
+  return (
+    <>
+      {" "}
+      {size === "Small" && <SmallButton onClick={onClick}>{children}</SmallButton>}
+      {size === "Medium" && <MediumButton onClick={onClick}>{children}</MediumButton>}
+      {size === "Large" && <LargeButton onClick={onClick}>{children}</LargeButton>}
+    </>
+  );
+};
 export default ButtonTest;
 
 const SmallButton = styled.div`
-    background-color: ${theme.color.main[500]};
-    width: 111px;
-    height: 52px;
-    border-radius: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    font-family: wanted;
-`
+  background-color: ${theme.color.main[500]};
+  width: 111px;
+  height: 52px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
 
 const MediumButton = styled.div`
-    background-color: ${theme.color.main[500]};
-    width: 140px;
-    height: 56px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    font-family: wanted;
-`
+  background-color: ${theme.color.main[500]};
+  width: 140px;
+  height: 56px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
 
 const LargeButton = styled.div`
-    background-color: ${theme.color.main[500]};
-    width: 600px;
-    height: 60px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 10px;
-    color: white;
-    font-family: wanted;
-`
+  background-color: ${theme.color.main[500]};
+  width: 615px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  color: white;
+`;
