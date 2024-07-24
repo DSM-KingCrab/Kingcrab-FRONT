5dfcb950f3a8457ebd389c9524098dbbbfd06d00
kimdayeon <kdiana080918@gmail.com>
2024-07-24 23:57:02 +0900

    로그인 페이지 수정

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -5,7 +5,7 @@ import { theme } from "../styles/theme";
 import KingCrabLogo from "../images/fluent-emoji-high-contrast_crab.png";
 import Eyes from "../images/mdi_eye.png";
 
-const Test = () => {
+const Login = () => {
   return (
     <StyledDiv>
       <StyledImage src={KingCrabLogo} alt="대게 로고" />
@@ -13,15 +13,19 @@ const Test = () => {
         <h1>
           <Span>대게</Span>
         </h1>
-        <h1>
-          <Span>대</Span>마고 <Span>게</Span>시판에 로그인 하세요
-        </h1>
+        <H1>
+          <h1>
+            <Span>대</Span>마고 <Span>게</Span>시판에 로그인 하세요
+          </h1>
+        </H1>
         <Input>
-          <p>아이디</p>
-          <input type="text" placeholder="아이디를 입력하세요" />
+          <Id>
+            <p>아이디</p>
+            <input type="text" placeholder="아이디를 입력하세요" />
+          </Id>
           <p>비밀번호</p>
           <input type="text" placeholder="비밀번호를 입력하세요" />
-          <Password src={Eyes} alt="눈" />
+          <Password src={Eyes} alt="비밀번호" />
         </Input>
         <LargeButton>
           <ButtonTest onClick={() => {}} size={"Large"}>
@@ -46,27 +50,34 @@ const StyledDiv = styled.div`
   align-items: center;
 `;
 const StyledImage = styled.img`
+  margin: 60px 0px 0px 0px;
   width: 200px;
   height: 200px;
 `;
 const Body = styled.div`
+  flex-direction: column;
   background-color: ${theme.color.white};
   width: 1344px;
   height: 692px;
   border-radius: 12px;
-  position: relative;
-  top: 22px;
+  display: flex;
+  justify-content: center;
   text-align: center;
   font-size: 20px;
+  margin: 22px 0px;
+  padding: 90px 0px 78px 0px;
   & h1 {
-    position: relative;
-    top: 50px;
+    line-height: 70px;
   }
 `;
+
+const H1 = styled.div`
+  margin: 0px 0px 50px 0px;
+`;
+
 const Span = styled.div`
   color: ${theme.color.main[500]};
   display: inline;
-  position: relative;
 `;
 const Input = styled.div`
   & input {
@@ -76,37 +87,34 @@ const Input = styled.div`
     font-size: 16px;
     padding: 0px 8px;
     border: 1px solid ${theme.color.gray[700]};
-    margin: 0px 0px 28px 0px;
   }
   & p {
-    margin: 0px 0px 20px 0px;
+    margin: 0px 0px 20px 374px;
     font-size: 16px;
     font-weight: bold;
-    position: relative;
-    right: 278px;
+    display: flex;
+    justify-content: flex-start;
   }
-
-  & p,
-  input {
-    position: relative;
-    top: 80px;
-  }
-`;
-
-const LargeButton = styled.div`
-  position: absolute;
-  top: 510px;
-  left: 364px;
 `;
 
 const Password = styled.div`
   width: 30px;
   height: 30px;
+  color: ${theme.color.gray[100]};
+`;
+
+const Id = styled.div`
+  margin: 0px 0px 28px 0px;
+`;
+
+const LargeButton = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 5px 0px 35px 0px;
 `;
 
 const SignUp = styled.div`
   font-size: 14px;
-  position: relative;
-  top: 146px;
 `;
-export default Test;
+
+export default Login;
